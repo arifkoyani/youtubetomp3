999bd96ee984cec6d8201f344e9e39ac60b6a2c2
arifkoyani <arifkoyani@gmail.com>
2025-05-12 16:30:38 +0200

    done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import YoutubeToMp3 from "@/components/YoutubeToMp3/YoutubeToMp3";
+
 export default function Home() {
   return (
     <div>
-      <h1>This is My main page </h1>
+      <YoutubeToMp3 />
     </div>
   );
 }
