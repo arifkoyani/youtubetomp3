47a1d95a21efba6be5799d9185c21f0e02ef77ba
Arifkoyani <128629913+arifkoyani@users.noreply.github.com>
2026-01-09 18:31:34 +0500

    yarn install

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,   // helps catch potential errors
+    swcMinify: true,         // faster builds
+  };
+  
+  export default nextConfig;
+  
